31fa91844677148723ba880381645d62928b79c2
Correct Username <correct@example.com>
2024-09-07 11:27:02 +0530

    added

--- index.js
+++ index.js
@@ -11,8 +11,9 @@ app.use(cors());
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
+
 
 app.get('/', (req, res) => {
     res.render('index');
